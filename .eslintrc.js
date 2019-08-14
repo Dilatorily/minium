bea1184cc865107aac2bb0e612c4005795556e24
Huy Dang Lê-Ngô <Dilatorily@users.noreply.github.com>
2019-08-14 11:10:01 -0700

    feat(typescript): setup TypeScript configuration (#40)
    Closes #38

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  extends: ['airbnb', 'plugin:import/typescript', 'plugin:prettier/recommended', 'prettier/react'],
   env: {
     jest: true,
   },
+  overrides: [
+    {
+      files: ['src/**/*.ts?(x)'],
+      extends: [
+        'airbnb',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+      ],
+      rules: {
+        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+      },
+    },
+  ],
 };
