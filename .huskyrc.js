3e2a6e013b17a1a478980b8f031f9f8221288692
Huy Dang Lê-Ngô <Dilatorily@users.noreply.github.com>
2020-06-21 17:10:36 -0700

    chore: update dependencies on Git pulls (#391)

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+    'post-merge': 'yarn',
     'pre-commit': 'lint-staged',
     'pre-push': 'yarn test && yarn build',
   },
